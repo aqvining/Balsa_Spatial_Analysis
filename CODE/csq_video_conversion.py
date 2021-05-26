c32f0cca65941e102b2df22dd0e0fd28a2085130
Alexander Vining <aqvining@ucdavis.edu>
2021-05-26 19:23:03 +0200

    libraries and initial exploration
    copying code from writing_full_vids_hard_drives.ipynb sent by carter, and modifying to attempt to understand his process. It is not going well.

---
+++ CODE/csq_video_conversion.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 26 18:25:33 2021
+
+@author: avining
+"""
+
+import fnv
+import fnv.reduce
+import fnv.file  
+import numpy as np
+import seaborn as sns
+import datetime as dt
+import time
+import pandas as pd
+from tkinter import filedialog
+from tkinter import *
+import os
+import matplotlib.pyplot as plt
+import glob
+import h5py
+import gc
+import shlex
+import pipes
+from subprocess import check_call
